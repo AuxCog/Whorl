42b4a27e6c4112689baf0229741b5fc48ed91bd0
Christopher Jay <christopher.jay@ct.gov>
2021-10-07 21:10:59 -0400

    Added InitialSettingValue attributes to settings properties.

--- Whorl/WhorlSettings.cs
+++ Whorl/WhorlSettings.cs
@@ -12,7 +12,7 @@ namespace Whorl
     public class WhorlSettings : ChangeTracker
     {
         public static WhorlSettings Instance { get; } = new WhorlSettings();
-        
+
         private WhorlSettings()
         {
         }
@@ -21,7 +21,7 @@ namespace Whorl
         {
             return this.GetType().GetProperty(name, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
         }
-        
+
         public object this[string propertyName]
         {
             get
@@ -59,245 +59,244 @@ namespace Whorl
             IsChanged = false;
         }
 
-        //Original value: 1
         private Int32 _animationRate;
+        [InitialSettingValue("1")]
         public Int32 AnimationRate
         {
             get { return _animationRate; }
             set { SetProperty(ref _animationRate, value); }
         }
 
-        //Original value: 40
         private Int32 _bufferSize;
+        [InitialSettingValue("40")]
         public Int32 BufferSize
         {
             get { return _bufferSize; }
             set { SetProperty(ref _bufferSize, value); }
         }
 
-        //Original value: DefaultPattern.config
+        [InitialSettingValue("CustomFolders")]
         [ReadOnly]
+        public string CustomDesignParentFolder { get; private set; }
+
+        [ReadOnly]
+        [InitialSettingValue("DefaultPattern.config")]
         public String DefaultPatternFileName { get; private set; }
 
-        //Original value: 809
         [ReadOnly]
+        [InitialSettingValue("809")]
         public Int32 DefaultPicHeight { get; private set; }
 
-        //Original value: 1596
         [ReadOnly]
+        [InitialSettingValue("1596")]
         public Int32 DefaultPicWidth { get; private set; }
 
-        //Original value: DesignPatterns.config
         [ReadOnly]
+        [InitialSettingValue("DesignPatterns.config")]
         public String DesignPatternsFileName { get; private set; }
 
-        //Original value: 150
         private Int32 _designThumbnailHeight;
+        [InitialSettingValue("150")]
         public Int32 DesignThumbnailHeight
         {
             get { return _designThumbnailHeight; }
             set { SetProperty(ref _designThumbnailHeight, value); }
         }
 
-        //Original value: DesignThumbnails
         [ReadOnly]
+        [InitialSettingValue("DesignThumbnails")]
         public String DesignThumbnailsFolder { get; private set; }
 
-        //Original value: CustomFolders
-        [InitialSettingValue("CustomFolders")]
-        [ReadOnly]
-        public string CustomDesignParentFolder { get; private set; }
-
-        //Original value: 4
         private Int32 _draftSize;
+        [InitialSettingValue("4")]
         public Int32 DraftSize
         {
             get { return _draftSize; }
             set { SetProperty(ref _draftSize, value); }
         }
 
-        //Original value: False
         private Boolean _exactOutline;
+        [InitialSettingValue("False")]
         public Boolean ExactOutline
         {
             get { return _exactOutline; }
             set { SetProperty(ref _exactOutline, value); }
         }
 
-        //Original value: C:\WhorlFilesDev
         private String _filesFolder;
+        [InitialSettingValue(@"C:\WhorlFiles")]
         public String FilesFolder
         {
             get { return _filesFolder; }
             set { SetProperty(ref _filesFolder, value); }
         }
 
-        //Original value: 1
         private Double _improvDamping;
+        [InitialSettingValue("1")]
         public Double ImprovDamping
         {
             get { return _improvDamping; }
             set { SetProperty(ref _improvDamping, value); }
         }
 
-        //Original value: 0.2
         private Double _improvisationLevel;
+        [InitialSettingValue("0.2")]
         public Double ImprovisationLevel
         {
             get { return _improvisationLevel; }
             set { SetProperty(ref _improvisationLevel, value); }
         }
 
-        //Original value: False
         private Boolean _improviseBackground;
+        [InitialSettingValue("False")]
         public Boolean ImproviseBackground
         {
             get { return _improviseBackground; }
             set { SetProperty(ref _improviseBackground, value); }
         }
 
-        //Original value: True
         private Boolean _improviseColors;
+        [InitialSettingValue("True")]
         public Boolean ImproviseColors
         {
             get { return _improviseColors; }
             set { SetProperty(ref _improviseColors, value); }
         }
 
-        //Original value: False
         private Boolean _improviseOnOutlineType;
+        [InitialSettingValue("False")]
         public Boolean ImproviseOnOutlineType
         {
             get { return _improviseOnOutlineType; }
             set { SetProperty(ref _improviseOnOutlineType, value); }
         }
 
-        //Original value: False
         private Boolean _improviseParameters;
+        [InitialSettingValue("False")]
         public Boolean ImproviseParameters
         {
             get { return _improviseParameters; }
             set { SetProperty(ref _improviseParameters, value); }
         }
 
-        //Original value: False
         private Boolean _improvisePetals;
+        [InitialSettingValue("False")]
         public Boolean ImprovisePetals
         {
             get { return _improvisePetals; }
             set { SetProperty(ref _improvisePetals, value); }
         }
 
-        //Original value: False
         private Boolean _improviseShapes;
+        [InitialSettingValue("False")]
         public Boolean ImproviseShapes
         {
             get { return _improviseShapes; }
             set { SetProperty(ref _improviseShapes, value); }
         }
 
-        //Original value: True
         private Boolean _improviseTextures;
+        [InitialSettingValue("True")]
         public Boolean ImproviseTextures
         {
             get { return _improviseTextures; }
             set { SetProperty(ref _improviseTextures, value); }
         }
 
-        //Original value: 1000
         private Int32 _maxLoopCount;
+        [InitialSettingValue("1000")]
         public Int32 MaxLoopCount
         {
             get { return _maxLoopCount; }
             set { SetProperty(ref _maxLoopCount, value); }
         }
 
-        //Original value: 100
         private Int32 _maxSavedImprovs;
+        [InitialSettingValue("100")]
         public Int32 MaxSavedImprovs
         {
             get { return _maxSavedImprovs; }
             set { SetProperty(ref _maxSavedImprovs, value); }
         }
 
-        //Original value: 1000
         private Double _minPatternSize;
+        [InitialSettingValue("1000")]
         public Double MinPatternSize
         {
             get { return _minPatternSize; }
             set { SetProperty(ref _minPatternSize, value); }
         }
 
-        //Original value: True
         private Boolean _optimizeExpressions;
+        [InitialSettingValue("True")]
         public Boolean OptimizeExpressions
         {
             get { return _optimizeExpressions; }
             set { SetProperty(ref _optimizeExpressions, value); }
         }
 
-        //Original value: PatternChoices.config
         [ReadOnly]
+        [InitialSettingValue("PatternChoices.config")]
         public String PatternChoicesFileName { get; private set; }
 
-        //Original value: 3000
         private Int32 _qualitySize;
+        [InitialSettingValue("3000")]
         public Int32 QualitySize
         {
             get { return _qualitySize; }
             set { SetProperty(ref _qualitySize, value); }
         }
 
-        //Original value: 5
         private Double _recomputeInterval;
+        [InitialSettingValue("5")]
         public Double RecomputeInterval
         {
             get { return _recomputeInterval; }
             set { SetProperty(ref _recomputeInterval, value); }
         }
 
-        //Original value: C:\GreetingCards\Final
         private String _renderFilesFolder;
+        [InitialSettingValue(@"C:\GreetingCards\Final")]
         public String RenderFilesFolder
         {
             get { return _renderFilesFolder; }
             set { SetProperty(ref _renderFilesFolder, value); }
         }
 
-        //Original value: 2000
         private Int32 _renderWidth;
+        [InitialSettingValue("2000")]
         public Int32 RenderWidth
         {
             get { return _renderWidth; }
             set { SetProperty(ref _renderWidth, value); }
         }
 
-        //Original value: 1
         private Double _replayIntervalSeconds;
+        [InitialSettingValue("1")]
         public Double ReplayIntervalSeconds
         {
             get { return _replayIntervalSeconds; }
             set { SetProperty(ref _replayIntervalSeconds, value); }
         }
 
-        //Original value: 0.1
         private Double _revolveRate;
+        [InitialSettingValue("0.1")]
         public Double RevolveRate
         {
             get { return _revolveRate; }
             set { SetProperty(ref _revolveRate, value); }
         }
 
-        //Original value: True
         private Boolean _saveDesignThumbnails;
+        [InitialSettingValue("True")]
         public Boolean SaveDesignThumbnails
         {
             get { return _saveDesignThumbnails; }
             set { SetProperty(ref _saveDesignThumbnails, value); }
         }
 
-        //Original value: 1090x1090:Square,1526x1090:7 x 5,1411x1090:11 x 8.5,1362x1090:10 x 8,1387x1090:11 x 14,727x1090:4 x 6,778x1090:5 x 7,856x1090:14 x 11,842x1090:8.5 x11
         private String _savedImageSizes;
+        [InitialSettingValue(@"1090x1090:Square,1526x1090:7 x 5,1411x1090:11 x 8.5,1362x1090:10 x 8,1387x1090:11 x 14,727x1090:4 x 6,778x1090:5 x 7,856x1090:14 x 11,842x1090:8.5 x11")]
         public String SavedImageSizes
         {
             get { return _savedImageSizes; }
@@ -307,7 +306,7 @@ namespace Whorl
 
         private String _imageSizes;
         /// <summary>
-        /// Comma separted list of AspectRatio:Description
+        /// Comma separated list of AspectRatio:Description
         /// </summary>
         public string ImageSizes
         {
@@ -322,32 +321,32 @@ namespace Whorl
             set { SetProperty(ref _parallelProcessing, value); }
         }
 
-        //Original value: 10
         private Int32 _smoothAnimationSteps;
+        [InitialSettingValue("10")]
         public Int32 SmoothAnimationSteps
         {
             get { return _smoothAnimationSteps; }
             set { SetProperty(ref _smoothAnimationSteps, value); }
         }
 
-        //Original value: 1
         private Double _spinRate;
+        [InitialSettingValue("1")]
         public Double SpinRate
         {
             get { return _spinRate; }
             set { SetProperty(ref _spinRate, value); }
         }
 
-        //Original value: C:\WhorlFilesDev\Textures
         private String _texturesFolder;
+        [InitialSettingValue(@"C:\WhorlFilesDev\Textures")]
         public String TexturesFolder
         {
             get { return _texturesFolder; }
             set { SetProperty(ref _texturesFolder, value); }
         }
 
-        //Original value: True
         private Boolean _traceMode;
+        [InitialSettingValue("True")]
         public Boolean TraceMode
         {
             get { return _traceMode; }
